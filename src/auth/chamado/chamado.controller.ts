263e26788b8091ebeea285c3efb8dbc978a1550f
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-12-02 11:08:47 -0300

    Update chamado.controller.ts

--- src/auth/chamado/chamado.controller.ts
+++ src/auth/chamado/chamado.controller.ts
@@ -1,61 +1,72 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  // UseGuards,
+} from '@nestjs/common';
 import { ChamadoService } from './chamado.service';
 import { CreateChamadoDto } from './dto/create_chamado.dto';
+// import { AuthGuard } from '../auth.guard';
 
+// @UseGuards(AuthGuard)
 @Controller('chamado')
 export class ChamadoController {
   constructor(private chamadoService: ChamadoService) {}
 
   @Post('/create')
   Create(@Body() data: CreateChamadoDto, @Req() req: any) {
-    try{
-      return this.chamadoService.create(data)
+    try {
+      return this.chamadoService.create(data);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   @Get('/')
   GetAll() {
     try {
-      return this.chamadoService.getAll()
+      return this.chamadoService.getAll();
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   @Get('/:id')
   GetOne(@Param('id') id: number) {
     try {
-      return this.chamadoService.getOne(id)
+      return this.chamadoService.getOne(id);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
-  @Get("/pesquisar")
+  @Get('/pesquisar')
   Search(@Query() pesquisa: any) {
-    const {all} = pesquisa
-    return 'chamado'
+    const { all } = pesquisa;
+    return 'chamado';
   }
 
   @Put('/atualizar/:id')
   Update(@Param('id') id: number, @Body() data: any) {
     try {
-      return this.chamadoService.update(id, data)
+      return this.chamadoService.update(id, data);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   @Delete('/delete/:id')
   Delete(@Param('id') id: number) {
     try {
-      return this.chamadoService.delete(id)
+      return this.chamadoService.delete(id);
     } catch (error) {
-      return error
+      return error;
     }
   }
-
-
 }
