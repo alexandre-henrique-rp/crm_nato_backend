82344b09f5b76ef2475e694ef8c1b1b08b8f502c
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-12-02 16:58:46 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser'; // Importação corrigida
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const Porta = process.env.NEST_PORT || 3000; // Adiciona um valor padrão para a porta
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove propriedades que não estão no DTO
-    forbidNonWhitelisted: true, // Lança erro se houver propriedades não definidas
-    transform: true // Converte automaticamente os tipos
-  }));
-  
+  const config = new DocumentBuilder()
+    .setTitle('Sis Nato Api')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove propriedades que não estão no DTO
+      forbidNonWhitelisted: true, // Lança erro se houver propriedades não definidas
+      transform: true, // Converte automaticamente os tipos
+    }),
+  );
+
   // Aumenta o limite do payload para 50MB
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
